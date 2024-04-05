efa85aa501e625f039a1e012954ac35835eeae0b
Rick Albaugh <albaughrick@gmail.com>
2024-04-05 14:23:56 -0400

    updated buy from master

--- Solana/Trade/buy.py
+++ Solana/Trade/buy.py
@@ -2,11 +2,13 @@ import asyncio
 import datetime
 import time
 import os
+import base58
 from solana.rpc.types import TokenAccountOpts
 from solders.pubkey import Pubkey
+from solders.keypair import Keypair
 from solana.rpc.commitment import Commitment, Confirmed, Finalized
 from solana.rpc.api import RPCException
-from solana.rpc.api import Client, Keypair
+from solana.rpc.api import Client
 from solana.rpc.async_api import AsyncClient
 from solders.compute_budget import set_compute_unit_price, set_compute_unit_limit
 from spl.token.instructions import create_associated_token_account, get_associated_token_address, close_account, \
@@ -17,15 +19,15 @@ from spl.token.core import _TokenCore
 from dotenv import load_dotenv
 
 # Load environment variables
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-# Access environment variables
-rpc_https_url = os.getenv('RPC_HTTPS_URL')
-private_key = os.getenv('PRIVATE_KEY')
+RPC_HTTPS_URL = os.getenv('RPC_HTTPS_URL')
+PRIVATE_KEY = os.getenv('PRIVATE_KEY')
+# Add other environment variables as needed
 
-# Initialize clients with environment variables
-async_solana_client = AsyncClient(rpc_https_url)
-solana_client = Client(rpc_https_url)
+async_solana_client = AsyncClient(RPC_HTTPS_URL)
+solana_client = Client(RPC_HTTPS_URL)
 
 LAMPORTS_PER_SOL = 1000000000
 MAX_RETRIES = 5
@@ -38,10 +40,11 @@ def getTimestamp():
         return currentTimeStamp
 
 async def get_transaction_with_timeout(client, txid, commitment=Confirmed, timeout=10):
+    # Wrap the synchronous get_transaction call in a coroutine
     loop = asyncio.get_event_loop()
     return await loop.run_in_executor(None, client.get_transaction, txid, "json")
 
-async def get_token_account(ctx, owner: Pubkey.from_string, mint: Pubkey.from_string):
+async def get_token_account(ctx, owner: Pubkey, mint: Pubkey):
     try:
         account_data = await ctx.get_token_accounts_by_owner(owner, TokenAccountOpts(mint))
         return account_data.value[0].pubkey, None
@@ -67,29 +70,35 @@ async def buy(solana_client, TOKEN_TO_SWAP_BUY, payer, amount):
                 False, balance_needed, Commitment("confirmed"))
 
             instructions_swap = make_swap_instruction(amount_in,
-                                                      WSOL_token_account,
-                                                      swap_associated_token_address,
-                                                      pool_keys,
-                                                      mint,
-                                                      solana_client,
+                                                      WSOL_token_account, 
+                                                      swap_associated_token_address, 
+                                                      pool_keys, 
+                                                      mint, 
+                                                      solana_client, 
                                                       payer)
-            params = CloseAccountParams(account=WSOL_token_account, dest=payer.pubkey(), owner=payer.pubkey(),
-                                        program_id=TOKEN_PROGRAM_ID)
-            closeAcc = close_account(params)
+            params = CloseAccountParams(account=WSOL_token_account, dest=payer.pubkey(), owner=payer.pubkey(), program_id=TOKEN_PROGRAM_ID)
+            closeAcc = (close_account(params))
             if swap_token_account_Instructions != None:
+                # recent_blockhash = solana_client.get_latest_blockhash(commitment="confirmed") #Without adding hash to instructions txn always goes through
+                # swap_tx.recent_blockhash = recent_blockhash.value.blockhash                
                 swap_tx.add(swap_token_account_Instructions)
+
+            #compute unit price and comute unit limit gauge your gas fees more explanations on how to calculate in a future article
 
             swap_tx.add(instructions_swap, set_compute_unit_price(25_232), set_compute_unit_limit(200_337), closeAcc)
             txn = solana_client.send_transaction(swap_tx, payer, Wsol_account_keyPair)
             txid_string_sig = txn.value
             if txid_string_sig:
                 print("Waiting For Transaction Confirmation .......")
+
                 print(f"Transaction Signature: https://solscan.io/tx/{txid_string_sig}")
+                # Await transaction confirmation with a timeout                
                 await asyncio.wait_for(
                     get_transaction_with_timeout(solana_client, txid_string_sig, commitment="confirmed", timeout=10),
                     timeout=15
                 )
                 print("Transaction Confirmed")
+                #return txid_string_sig
                 return True
         except asyncio.TimeoutError:
             print("Transaction confirmation timed out. Retrying...")
@@ -108,10 +117,12 @@ async def buy(solana_client, TOKEN_TO_SWAP_BUY, payer, amount):
     return False
 
 async def main():
-    token_toBuy = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"  # Enter token you wish to buy here
-    payer = Keypair.from_base58_string(private_key)
+    secret_key_bytes = base58.b58decode(os.getenv('PRIVATE_KEY'))
+    payer = Keypair.from_bytes(secret_key_bytes)
+    
     print(payer.pubkey())
-    buy_transaction = await buy(solana_client, token_toBuy, payer, 0.01)  # Enter amount of sol you wish to spend
+    token_toBuy = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"  # Enter the token you wish to buy here
+    buy_transaction = await buy(solana_client, token_toBuy, payer, 0.01)  # Enter the amount of SOL you wish to spend
     print(buy_transaction)
 
 asyncio.run(main())
